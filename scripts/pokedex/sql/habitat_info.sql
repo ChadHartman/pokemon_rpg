7badce760f81d45364c08c7b40b5b17dba053d77
chadhartman <chad+github@tealium.com>
2019-10-20 15:29:20 -0700

    Setting up pages

--- scripts/pokedex/sql/habitat_info.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    pokemon_habitats.identifier AS habitat,
-    pokemon_species.identifier AS pokemon,
-    pokemon_species.capture_rate AS appear
-FROM pokemon_species 
-    INNER JOIN pokemon_habitats
-        ON pokemon_species.habitat_id = pokemon_habitats.id
-WHERE pokemon_habitats.id = ?
-ORDER BY habitat, capture_rate DESC
